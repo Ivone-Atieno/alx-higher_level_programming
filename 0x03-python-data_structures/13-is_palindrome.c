571e1b3e9065ebeb93bf7f4b41842fbec8f10e31
Ivone-Atieno <ivoneowuor@gmail.com>
2023-09-10 10:47:11 +0000

    python assignment 2 and 3

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * main - main entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  listint_t *head;
+
+  return (0);
+}
